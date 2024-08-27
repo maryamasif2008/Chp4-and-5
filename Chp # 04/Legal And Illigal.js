e7146b978ae81f6e076aaa5d4e83e43351bcad79
maryamasif2008 <marayam.asif123@gmail.com>
2024-08-28 00:03:11 +0500

    added index.html file

---
+++ Chp # 04/Legal And Illigal.js
@@ -0,0 +1,21 @@
+                          /* Question 01 */
+var a = 2 , b = 4 , c = 6;
+document.write("var")
+
+                          /* Question 02 */
+//Legal Variable
+var num1 = 10;
+var a = 'maryam';
+var variable$ = 30;
+var var123 = 'asif';
+var variable_name = 'candy';
+
+//Illegal Variable
+var @variable = 4;
+var 123var = 'ali';
+var var-iable = 67;
+var name-variable = "ahmed";
+var %variable = 5;
+
+                         /* Question 03 */
+
---
+++ Chp # 04/Math Expression.js
@@ -0,0 +1,131 @@
+                          //CHAPTER 05
+
+                          /* Question 01 */
+var message = " <h1> Question 01 </h1> \n Sum 3 and 5 is ";
+  document.write(message)
+var num1 = 3;
+var num2 = 5;
+  document.write(num1 +  num2)
+                                     
+                          /* Question 02 */
+var message = " <h1> Question 02 </h1> \n sub   = ";
+  document.write(message)
+var num1 = 8;
+var num2 = 6;
+  document.write( num1 - num2 )
+                         
+var message = " <br> mult   = ";
+  document.write(message)  
+var num1 = 9;
+var num2 = 3;
+  document.write( num1 * num2)
+                         
+var message = " <br> div   = ";
+  document.write(message) 
+var num1 = 3;
+var num2 = 4;
+  document.write( num1 / num2)
+                         
+var message = " <br> mod   = ";
+  document.write(message) 
+var num1 = 9;
+var num2 = 7;
+  document.write( num1 % num2)
+                         
+                          /* Question 03 */
+var message = " <h1> Question 03 </h1> \n Value after variable declaration is undefined <br> Initial value: 5 <br> Value after increment is: 6 <br> value after addition is: 13 <br> Value after decrement is: 12 <br> The reminder is: 0";
+  document.write(message)
+                                                  
+                          /* Question 04 */
+var message = " <h1> Question 04 </h1> \n Total cost to buy 5 tickets to a movie is 3000KPR";
+  document.write(message)
+                                                 
+                          /* Question 05 */
+var message = " <h1> Question 05 </h1> \n <h3> Table of 4 </h3> \n 4x1=4 <br> 4x2=8 <br> 4x3=12 <br> 4x4=16 <br> 4x5=20 <br> 4x6=24 <br> 4x7=28 <br> 4x8=32 <br> 4x9=36 <br> 4x10=40";
+  document.write(message)
+                         
+                         /* Question 06 */
+var message = " <h1> Question 06 </h1> \n 25<sup>0</sup>C is 77<sup>0</sup>F <br> 70<sup>0</sup>F is 21.11111111111111<sup>0</sup>C ";
+  document.write(message)
+                         
+                         /* Question 07 */
+var message = " <h1> Question 07 </h1> \n <h2> Shopping Cart </h2> ";
+  document.write(message) 
+                           
+var message = " Price of item 1 is ";
+  document.write(message) 
+                         
+var item1= 650;
+  document.write(item1) 
+                                            
+var message = " <br> Quality of item 1 is ";
+  document.write(message) 
+                         
+var item1= 3;
+  document.write(item1) 
+                         
+var message = " <br> Price of item 2 is ";
+  document.write(message) 
+                         
+var item2= 100;
+  document.write(item2) 
+                         
+var message = " <br> Quality of item 2 is ";
+  document.write(message) 
+                         
+var item2= 7;
+  document.write(item2) 
+                         
+var message = " <br> <br>  Total cost of order is ";
+  document.write(message) 
+                         
+var total= 2750;
+  document.write(total) 
+                         
+                         /* Question 08 */
+var message = " <h1>Question 08</h1> \n <h2>Marks Sheet</h2> ";
+  document.write(message) 
+                          
+var message = " \n Total marks: 980 <br> Markobtained: 804  ";
+  document.write(message) 
+                           
+var message = " Totalmarks: 980 <br> Marksobtained: 804 <br> Percentage: 82.0408163265306%  ";
+  document.write(message) 
+                         
+                         /* Question 09 */
+var message = " <h1>Question 09</h1> \n <h2>Currency in PKR</h2> ";
+  document.write(message) 
+                                                                
+var message = " Total Currency in PKR: 1748 ";
+  document.write(message) 
+                                                             
+                         /* Question 10 */
+var message = " <h1>Question 10</h1> ";
+  document.write(message) 
+                                          
+var initial_value = 8; 
+  document.write( (initial_value + 5) * 10 / 2)
+                         
+                         /* Question 11 */
+var message = " <h1>Question 11</h1> \n <h2>Age Calculator</h2> ";
+  document.write(message) 
+                                                                                 
+var message = " <br> Current year: 2024 <br> Birth year: 2008 <br> Your age is: 16 ";
+  document.write(message) 
+                                                              
+                         /* Question 12 */
+var message = " <h1>Question 12</h1> \n <h2>The Geometrizer</h2> ";
+  document.write(message) 
+                                                                               
+var message = " <br> Radius of a circle: 20 <br> The circumference: 125.6799999999999 <br> The area is: 1256.8 ";
+  document.write(message) 
+                                          
+                         /* Question 13 */
+var message = " <h1>Question 13</h1> \n <h2>The Liftime Supply Calculator</h2> ";
+  document.write(message) 
+                                                                               
+var message = " <br>  Favourite Snake: Chocolate <br> Current age: 16 <br> Extimated Mximum Age: 66 <br> Amount of snake per day: 3 <br> You will need 150 chocolate to last you until the rip old age of 65 ";
+  document.write(message) 
+                              
+                              
+                                                                         
